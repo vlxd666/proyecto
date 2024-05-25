a6810e432f6ee70b48668eb7c6e552f7b83a310f
vlxd666 <166763189+vlxd666@users.noreply.github.com>
2024-05-24 22:36:23 -0500

    Add files via upload
    Signed-off-by: vlxd666 <166763189+vlxd666@users.noreply.github.com>

---
+++ footer.php
@@ -0,0 +1,26 @@
+<footer>
+        <div class="footer-content">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, dicta, amet doloremque ut ipsum iusto odit hic maxime optio nostrum architecto, quis quasi perspiciatis nulla aliquam quo. Eos, iusto sunt?
+            Quis totam odit nesciunt, minus repellendus cum voluptates aliquam impedit reiciendis labore pariatur explicabo, corporis vel eius minima officiis quia, dolore accusantium modi. Velit fuga ipsa accusamus repudiandae sunt atque!
+            Facilis ratione nemo aperiam? A quisquam esse ipsum, laborum culpa voluptate nemo non, illo velit sint dolor iure voluptates quia aspernatur. Iusto expedita vel delectus magni tempora facere facilis dolorum.
+            Fugit, officia, exercitationem reiciendis repudiandae nisi officiis dicta culpa reprehenderit provident maiores soluta amet cupiditate error incidunt suscipit quidem minima doloremque praesentium aliquid. Mollitia asperiores officiis adipisci tenetur blanditiis. Maiores.
+            Officia veritatis, est qui officiis voluptates soluta ducimus perspiciatis alias architecto impedit placeat cupiditate incidunt expedita nisi numquam. Quod blanditiis culpa assumenda quo id autem tempore quis non vitae aliquam!
+            Deleniti at repellendus iure, quis, autem dolores tenetur consequuntur a in, sed deserunt assumenda praesentium beatae ipsa commodi architecto ea reiciendis quas ad delectus soluta eligendi sint ipsam! Sunt, illo?
+            Commodi nemo et eos eveniet doloremque, iusto ex aliquid quo. Iste itaque voluptatum rerum ipsam praesentium optio quia nihil nesciunt quas dignissimos harum, magnam omnis facere quibusdam ex porro non!
+            Error aperiam nisi quia voluptatem velit esse modi, ipsum perferendis numquam placeat fugiat sapiente at libero reiciendis facere quibusdam tempore quasi tenetur ducimus dignissimos deleniti quod maxime delectus id. Eligendi!
+            Atque, non ea mollitia excepturi minus facere ex illo modi commodi, deleniti sed eum ipsa quos vel quae porro ratione quis molestias veritatis suscipit cum nobis at? Quia, doloribus aliquam.
+            Suscipit, obcaecati! Assumenda ad quia numquam dolor ratione debitis ab magni, ex deserunt qui laborum, a minus labore iusto fugiat officia aliquid impedit molestiae alias, consequatur distinctio architecto? Repellat, magnam.</p>
+            <ul class="social-icons">
+              <li><a class="facebook" href="https://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="twitter" href="https://x.com"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="dribbble" href="https://dribbble.com"><i class="fa fa-dribbble"></i></a></li>
+              <li><a class="linkedin" href="https://ec.linkedin.com"><i class="fa fa-linkedin"></i></a></li>   
+            </ul>
+            <ul class="payment-methods">
+                <li><i class="fa fa-cc-visa"></i></li>
+                <li><i class="fa fa-cc-mastercard"></i></li>
+                <li><i class="fa fa-cc-paypal"></i></li>
+                <li><i class="fa fa-cc-amex"></i></li>
+            </ul>
+        </div>
+    </footer>
