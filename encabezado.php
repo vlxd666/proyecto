08aa29606f9ad8f5dbb80eabc3f18649403e7dd4
vlxd666 <166763189+vlxd666@users.noreply.github.com>
2024-05-24 22:50:41 -0500

    Update encabezado.php
    Signed-off-by: vlxd666 <166763189+vlxd666@users.noreply.github.com>

--- encabezado.php
+++ encabezado.php
@@ -3,7 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="icon" href="img\iconpestaña.png">
     <link rel="stylesheet" href="style.css">
     <title>De la cocina al corazón</title>
 </head>
+<header>
+        <h1>DE LA COCINA AL CORAZÓN</h1>
+        <input type="checkbox" id="check">
+        <label for="check" class="mostrar-menu">
+         &#8801   
+        </label>
+        <nav class="menu">
+            <a href="index.php">Inicio</a>
+            <a href="nosotros.php">Sobre nosotros</a>
+            <a href="blog.php">Blog</a>
+            <a href="consulta.php"><img src="img\anadir-al-carrito.png" alt="20px" width="20px"></a>
+            <label for="check" class="esconder-menu">
+                &#215
+            </label>
+        </nav>
+    </header>
