95cc42a6e4ddfc235d738c3ef1348d0764214009
lxzbth <lizbethaucatoma9@gmail.com>
2024-05-29 20:35:40 -0500

    Update nosotros.php
    Signed-off-by: lxzbth <lizbethaucatoma9@gmail.com>

---
+++ nosotros.php
@@ -0,0 +1,21 @@
+<?php include ("encabezado.php")?>
+      <div class="banner-pan">
+          <h2>Sobre Nosotros</h2>
+      </div>
+
+<section class="cont-nosotros">
+        
+        <div class="foto">
+            <img src="imagenes/hola.jpg">
+        </div>
+        <div class="texto">
+            <h2>Quienes somos?</h2>
+            <p>
+            La espiga dorada , combinamos tradición y pasión para crear productos artesanales de la más alta calidad. Desde [año de fundación], utilizamos ingredientes frescos y naturales para ofrecer sabores auténticos y memorables.
+            Cada producto es elaborado a mano siguiendo métodos tradicionales, desde crujientes baguettes hasta esponjosos croissants. Nos enorgullece colaborar con proveedores locales para garantizar la frescura y sostenibilidad de nuestros ingredientes.
+            Somos parte de sus momentos especiales, brindando experiencias únicas con cada bocado. En[ la espiga dorada ], no solo horneamos pan; creamos recuerdos.Visítenos y descubra por qué somos la elección preferida de tantos amantes del buen pan.
+            </p>  
+        </div>
+</section>
+
+<?php include ("footer.php")?>
