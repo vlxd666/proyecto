81d64b2724b121f97fe8ca26a5c3f9010a0f6de4
Majesdar <stevenalmache91@gmail.com>
2024-05-30 23:36:04 -0500

    Update index.php
    Signed-off-by: Majesdar <stevenalmache91@gmail.com>

--- index.php
+++ index.php
@@ -1,7 +1,83 @@
 <?php include("encabezado.php") ?>
 <body>
-    <div class="contenedor"> </div>
+    <div class="index-cont"> 
+        
+        <div class="index-fondo">
+            <h3>La mejor opcion para tus desayunos</h3>
+            <h2>LA ESPIGA</h2>
+            <h2>DORADA</h2>
+        </div>
+        
+        <div class="index-info">
+            <div class="info-foto">
+                <img src="imagenes/panadero.jpg">
+            </div>
+            
+            <div class="info">
+                <h2>UNA GRAN VARIEDAD DE SABORES PARA LOS PALADARES MAS EXIGENTES</h2>
+                <h3>Te invitamos a degustar productos del horno a tu mesa, con los mas altos estandares de calidad y precios accesibles a nuestra 
+                comunidad, contamos con 10 años de experiencia en el mercado, apoyando el desarrollo de la industria.</h3>
+            </div>
+            
+        </div>
+        
+        <hr>
+        
+        <h1>PROCESOS DE CALIDAD</h1><br>
+        
+        <div class="index-info2">
+            <div class="img-proc">
+                <img src="imagenes/proceso.jpg">
+                <h2>PROFESIONALES CALIFICADOS</h2>
+            </div>
+            
+            <div class="img-proc">
+                <img src="imagenes/proceso1.jpg">
+                <h2>PASION POR EL CAFE</h2>
+            </div>
+            
+            <div class="img-proc">
+                <img src="imagenes/proceso2.jpg">
+                <h2>PASTELES PERSONALIZADOS</h2>
+            </div>
+            
+            <div class="img-proc">
+                <img src="imagenes/proceso3.jpg">
+                <h2>SERVICIO A DOMICILIO</h2>
+            </div>
+        </div>
+        
+        <hr>
+
+        <h1>NUESTRAS ESPECIALIDADES</h1><br>
+        <center>
+            <div class="index-info2-cont">
+            
+                <div class="index-info2">
+
+                    <div class="info-foto2">
+                        <img src="imagenes/pan1.jpg">
+                    </div>
+                    <div class="info2">
+                        <h2>HACER PAN ES UN ARTE, OFRECEMOS PAN FRESCO TODOS LOS DIAS</h2>
+                        <h3>Nos enorgullece estar siempre en tu mesa desde el 2000 renovándonos y creciendo junto a cada familia ecuatoriana.</h3>
+                    </div>
+
+                </div>
+                <div class="index-info2">
+
+                    <div class="info2">
+                        <h2>HACER PAN ES UN ARTE, OFRECEMOS PAN FRESCO TODOS LOS DIAS</h2>
+                        <h3>Nos enorgullece estar siempre en tu mesa desde el 2000 renovándonos y creciendo junto a cada familia ecuatoriana.</h3>
+                    </div>
+                    <div class="info-foto2">
+                        <img src="imagenes/pan1.jpg">
+                    </div>
+                </div>
+            </div>
+        </center>
+        
+    </div>
     
 </body>
 <?php include("footer.php") ?>
-</html>
